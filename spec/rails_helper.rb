e0ef0aefcb047a6d3f81bb7c56e07cd91f15623a
ctneal91 <ctnealherman@gmail.com>
2026-01-15 21:08:13 -0600

    Configure RSpec with SimpleCov for 90% coverage
    - Add RSpec, SimpleCov, FactoryBot, Shoulda-Matchers
    - Add WebMock and VCR for HTTP mocking
    - Add Faraday for HTTP client
    - Configure 90% minimum coverage, 80% per-file
    - Set up VCR cassette recording

---
+++ spec/rails_helper.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+require "webmock/rspec"
+
+# Load support files
+Rails.root.glob("spec/support/**/*.rb").sort_by(&:to_s).each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+
+RSpec.configure do |config|
+  config.fixture_paths = [Rails.root.join("spec/fixtures")]
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
+end
+
+# Shoulda Matchers
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+# VCR configuration
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = false
+  config.default_cassette_options = {
+    record: :once,
+    match_requests_on: [:method, :uri]
+  }
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+require "simplecov"
+
+SimpleCov.start "rails" do
+  enable_coverage :branch
+
+  add_filter "/spec/"
+  add_filter "/config/"
+  add_filter "/vendor/"
+
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Services", "app/services"
+  add_group "Jobs", "app/jobs"
+
+  minimum_coverage 90
+  minimum_coverage_by_file 80
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  config.order = :random
+  Kernel.srand config.seed
+end
